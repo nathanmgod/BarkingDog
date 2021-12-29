7332a0c5d590ed1808f2022950c6ef8564f176b9
Nathan de Moura Godoy <nathanmgodoy@gmail.com>
2021-12-29 09:36:55 -0300

    Add files via upload

---
+++ BarkingDog/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(BarkingDog.shouldWakeUp(true, 1));
+        System.out.println(BarkingDog.shouldWakeUp(true, -2));
+        System.out.println(BarkingDog.shouldWakeUp(false, 1));
+        System.out.println(BarkingDog.shouldWakeUp(true, 8));
+
+    }
+}
